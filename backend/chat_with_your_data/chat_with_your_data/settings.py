5918443e43940ffc0b3576047e45c543f63b751b
Mats Klein <mats.klein@naymspace.de>
2023-08-26 09:56:14 +0200

    configure postgres

--- backend/chat_with_your_data/chat_with_your_data/settings.py
+++ backend/chat_with_your_data/chat_with_your_data/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'chat_with_your_data.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "postgres",
+        "PORT": "5432",
     }
 }
 
