56c7c2d7133f13c743908f4f226fac5e0058c81d
Mats Klein <mats.klein@naymspace.de>
2023-08-26 10:18:37 +0200

    deploy backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,10 @@ cd /app
 pip install --upgrade pip
 pip3 install -r requirements.txt
 
+cd chat_with_your_data
+
+python manage.py migrate
+
 gunicorn --bind :8000 --workers 2 chat_with_your_data.wsgi
 
 exec "$@"
