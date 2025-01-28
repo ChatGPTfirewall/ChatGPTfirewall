083ace95a636884acd3419c1993c0efc6c27cc1c
Jannick <jannick.lindner@hotmail.de>
2025-01-28 18:20:43 +0100

    feat: Show user remaining API Calls

--- frontend/src/components/common/LoginButton/LoginButtonStyles.tsx
+++ frontend/src/components/common/LoginButton/LoginButtonStyles.tsx
@@ -2,13 +2,23 @@ import { makeStyles, tokens } from '@fluentui/react-components';
 
 const LoginButtonStyles = makeStyles({
   userText: {
-    fontWeight: tokens.fontWeightMedium,
-    marginRight: tokens.spacingHorizontalL
+    fontWeight: tokens.fontWeightBold,
   },
   loginButtonContainer: {
     display: 'flex',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+    flexDirection: "row"
+  },
+  mailText: {
+    fontWeight: tokens.fontWeightRegular,
+    fontSize: tokens.fontSizeBase200,
+  },
+  UserDetailsContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: "column",
+    marginRight: tokens.spacingHorizontalL
+  },
 });
 
 export default LoginButtonStyles;
