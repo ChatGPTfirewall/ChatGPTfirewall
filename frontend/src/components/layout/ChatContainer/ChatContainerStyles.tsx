7984325ff6b5ec71c5dce101565398d747d4539f
Jannick <jannick.lindner@hotmail.de>
2025-03-10 19:28:54 +0100

    style: adjust size for taller screens

--- frontend/src/components/layout/ChatContainer/ChatContainerStyles.tsx
+++ frontend/src/components/layout/ChatContainer/ChatContainerStyles.tsx
@@ -11,7 +11,7 @@ const ChatContainerStyles = makeStyles({
     paddingBottom: tokens.spacingVerticalL,
     marginLeft: tokens.spacingHorizontalXL,
     marginRight: tokens.spacingHorizontalXL,
-    height: 'calc(-220px + 92vh)',
+    height: 'calc(-225px + 92vh)',
     overflowY: 'auto'
   }
 });
